db78c4baff019f199b2bccca1406997d9ddc344e
Satyam Mishra <109024495+satyam-mishra-pce@users.noreply.github.com>
2023-07-30 20:55:21 +0530

    Update leetcode.js
    First initialization for getting the histogram data

--- leetcode.js
+++ leetcode.js
@@ -1,66 +1,33 @@
 //graphql query
 const query = `
-  query getUserProfile($username: String!) {
-    allQuestionsCount {
-      difficulty
-      count
-    }
-    matchedUser(username: $username) {
-      contributions {
-        points
-      }
-      profile {
-        reputation
-        ranking
-      }
-      submissionCalendar
-      submitStats {
-        acSubmissionNum {
-          difficulty
-          count
-          submissions
-        }
-        totalSubmissionNum {
-          difficulty
-          count
-          submissions
-        }
-      }
-    }
+  query contestRatingHistogram {
+  contestRatingHistogram {
+    userCount
+    ratingStart
+    ratingEnd
+    topPercentage
   }
+}
 `;
 
 
 // format data 
 const formatData = (data) => {
     let sendData =  {
-        totalSolved: data.matchedUser.submitStats.acSubmissionNum[0].count,
-        totalSubmissions:  data.matchedUser.submitStats.totalSubmissionNum,
-        totalQuestions: data.allQuestionsCount[0].count,
-        easySolved: data.matchedUser.submitStats.acSubmissionNum[1].count,
-        totalEasy: data.allQuestionsCount[1].count,
-        mediumSolved: data.matchedUser.submitStats.acSubmissionNum[2].count,
-        totalMedium: data.allQuestionsCount[2].count,
-        hardSolved: data.matchedUser.submitStats.acSubmissionNum[3].count,
-        totalHard: data.allQuestionsCount[3].count,
-        ranking: data.matchedUser.profile.ranking,
-        contributionPoint: data.matchedUser.contributions.points,
-        reputation: data.matchedUser.profile.reputation,
-        submissionCalendar: JSON.parse(data.matchedUser.submissionCalendar)
+        receivedData: data
     }
     return sendData;
 }
 
 //fetching the data
 exports.leetcode = (req, res) => {
-    let user = req.params.id;
     fetch('https://leetcode.com/graphql', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Referer': 'https://leetcode.com'
+            'Referer': 'https://leetcode.com/satyam_mishra13/'
         }, 
-        body: JSON.stringify({query: query, variables: {username: user}}),
+        body: JSON.stringify({query: query}),
     
     })
     .then(result => result.json())
