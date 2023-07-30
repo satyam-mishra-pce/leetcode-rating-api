7ee9af31ebe8a935dd46d9df6c13f024007b6f2c
Satyam Mishra <109024495+satyam-mishra-pce@users.noreply.github.com>
2023-07-30 21:58:26 +0530

    Corrected API
    API now returns the ratings of the user since the first contest attended to the latest contest.

--- leetcode.js
+++ leetcode.js
@@ -1,33 +1,45 @@
+const fetch = require("cross-fetch");
+
 //graphql query
 const query = `
-  query contestRatingHistogram {
-  contestRatingHistogram {
-    userCount
-    ratingStart
-    ratingEnd
-    topPercentage
-  }
-}
-`;
+    query userContestRankingInfo($username: String!) {
+        userContestRankingHistory(username: $username) {
+            rating
+            ranking
+            contest {
+                title
+                startTime
+            }
+            finishTimeInSeconds
+
+        }
+    }
+`
 
 
 // format data 
 const formatData = (data) => {
-    let sendData =  {
-        receivedData: data
+    const historyObjects = data.userContestRankingHistory;
+    for (let i = 0; i < historyObjects.length; i++) {
+      if (historyObjects[i].ranking != 0) {
+        historyObjects.splice(0, Math.max(i - 1, 0));
+        break;
+      }
     }
-    return sendData;
+    return historyObjects;
+  
 }
 
 //fetching the data
 exports.leetcode = (req, res) => {
+    let user = req.params.id;
     fetch('https://leetcode.com/graphql', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Referer': 'https://leetcode.com/satyam_mishra13/'
+            'Referer': 'https://leetcode.com'
         }, 
-        body: JSON.stringify({query: query}),
+        body: JSON.stringify({query: query, variables: {username: user}, operationName: "userContestRankingInfo"}),
     
     })
     .then(result => result.json())
